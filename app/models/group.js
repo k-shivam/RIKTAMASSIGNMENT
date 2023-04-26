ac89d8c11e3acc7dee27fb4aa41507b00108bab1
k-shivam <42085548+k-shivam@users.noreply.github.com>
2023-04-26 22:08:30 +0530

    Add files via upload

---
+++ app/models/group.js
@@ -0,0 +1,24 @@
+// Import required modules
+const mongoose = require('mongoose');
+
+// Define the group schema
+const groupSchema = new mongoose.Schema({
+  name:{ 
+      type: String, 
+      required: true 
+    },
+  description:{ 
+      type: String, 
+      required: true 
+    },
+  members:[
+      { 
+          type: mongoose.Schema.Types.ObjectId, 
+          ref: 'User' 
+        }
+    ]
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
---
+++ app/models/groupMembers.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const GroupMessageSchema = new mongoose.Schema({
+  groupId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Group',
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+}, { timestamps: true });
+
+const GroupMessage = mongoose.model('GroupMessage', GroupMessageSchema);
+
+module.exports = GroupMessage;
